fb1a0eb3fe7885a826fad2cf12a157f05f9b1116
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-07-05 08:55:52 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
-/** main - prints putchar
+/*
+ * main - prints putchar
  * Return : always 0
  */
 
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +17,5 @@ int main (void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0) ;
-}	
+	return (0);
+}
