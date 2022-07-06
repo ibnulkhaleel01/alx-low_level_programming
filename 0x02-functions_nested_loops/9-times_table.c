6ec2a6de1acfb65a490e9f5a31e310dd1be1641a
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-07-05 20:12:01 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 void times_table(void)
 {
 	int x, y, k;
-	
+
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
@@ -17,8 +17,8 @@ void times_table(void)
 				_putchar(k + '0');
 			if (y != 0 && k < 10)
 			{
-				_putchar(',');											
-				_putchar(' ');											
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
 			} else if (k >= 10)
@@ -26,9 +26,8 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');									
-			}
-																
+				_putchar((k % 10) + '0');
+			}									
 		}
 		_putchar('\n');
 	}
