d2c3ea7868731c3fdbfec11aae0652e5eb45a178
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-07-05 18:12:19 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ * @c: a character argument
+ * return: returns 0 and 1 depending on conditions 
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+}
