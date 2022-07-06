1d8db041927aa622df178add9a3c205d27172f11
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-07-05 17:43:40 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * islower - checks if the character is lowecase
+ * Return : returns 1 and 0 depending on condition
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
