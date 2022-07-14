a2250f8949cab47bfbed4a8a8e8e157c8db17a08
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-07-14 12:03:50 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,11 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125}
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
-
 	count++;
-
 	while (*(s + count) != '\0')
 	{
 		for (i = 0; i < 13; i++)
@@ -23,12 +21,11 @@ char *cap_string(char *s)
 			if (*(s + count) == sep_words[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32
-						break;
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
 			}
 		}
 		count++;
 	}
-
 	return (s);
 }
