478c50d4bae6467fb8b3f6bf1790efcfa7d24a95
Qozeem Ibrahim <qaasimibrahim2001@gmail.com>
2022-08-05 03:52:33 -0700

    Dog file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,10 +4,9 @@
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
- * @age: The age of the dog. 
+ * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-
 struct dog
 {
 	char *name;
@@ -18,7 +17,6 @@ struct dog
 /**
  * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
